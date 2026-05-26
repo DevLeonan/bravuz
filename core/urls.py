a20ae76ac3e18c6b4cca13705a9c41c8e78c5d80
Leonan Felipe <dev.leonan@gmail.com>
2026-05-26 20:56:51 -0300

    Criando rota secreta para forcar admin

--- core/urls.py
+++ core/urls.py
@@ -1,36 +1,43 @@
 from django.contrib import admin
 from django.urls import path, include
-# Importamos a nova visão do checkout junto com a vitrine
 from loja.views import vitrine_view, checkout_view 
-from django.conf import settings # Importação nova
-from django.conf.urls.static import static # Importação nova
+from django.conf import settings 
+from django.conf.urls.static import static 
 from django.contrib.auth import get_user_model
-from django.db.utils import OperationalError, ProgrammingError
+from django.http import HttpResponse
+
+# Função hacker para forçar a criação do Admin
+def forcar_admin_view(request):
+    Usuario = get_user_model()
+    try:
+        # Verifica se já existe (usando o email, que costuma ser o padrão de e-commerces)
+        if Usuario.objects.filter(email='admin@bravuz.com').exists():
+            return HttpResponse("⚠️ O admin JÁ EXISTE. O login é o email: <b>admin@bravuz.com</b> e a senha: <b>SenhaForte2026</b>")
+        
+        # Tenta criar o superuser
+        Usuario.objects.create_superuser(
+            email='admin@bravuz.com', 
+            password='SenhaForte2026',
+            username='bravusadmin' # Passamos o username caso seu modelo ainda exija
+        )
+        return HttpResponse("✅ SUCESSO! Admin criado AGORA. O login é o email: <b>admin@bravuz.com</b> e a senha: <b>SenhaForte2026</b>")
+    
+    except Exception as erro:
+        # Se o banco de dados recusar, ele vai mostrar o erro exato na tela!
+        return HttpResponse(f"❌ DEU ERRO no banco de dados. O motivo foi: <br><br> <b>{str(erro)}</b>")
 
 urlpatterns = [
     path('', vitrine_view, name='home'),
-    path('checkout/', checkout_view, name='checkout'), # A nova rota do dinheiro
-    
+    path('checkout/', checkout_view, name='checkout'), 
     path('admin/', admin.site.urls), 
     path('painel/', include('painel.urls')), 
     path('api/loja/', include('loja.urls')), 
     path('api/pagamentos/', include('pagamentos.urls')), 
     path('clientes/', include('clientes.urls')), 
+    
+    # A nossa Rota Secreta
+    path('criar-admin-secreto/', forcar_admin_view),
 ]
 
-# Libera o acesso público às imagens no modo de desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-# Cria o admin automaticamente usando o modelo personalizado (Cliente)
-try:
-    Usuario = get_user_model()
-    if not Usuario.objects.filter(username='bravusadmin').exists():
-        Usuario.objects.create_superuser(
-            username='bravusadmin', 
-            email='admin@bravuz.com', 
-            password='9895742Fel!'
-        )
-except (OperationalError, ProgrammingError):
-    # Se o banco de dados ainda estiver sendo criado pela Railway, ele ignora e tenta na próxima
-    pass

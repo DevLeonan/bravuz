98a610b6eb4a9872831308fa3cf0fc1803412107
Leonan Felipe <dev.leonan@gmail.com>
2026-05-26 21:01:26 -0300

    Auto login marreta

--- core/urls.py
+++ core/urls.py
@@ -3,26 +3,33 @@ from django.urls import path, include
 from loja.views import vitrine_view, checkout_view 
 from django.conf import settings 
 from django.conf.urls.static import static 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
+from django.shortcuts import redirect
 from django.http import HttpResponse
 
 def forcar_admin_view(request):
     Usuario = get_user_model()
-    try:
-        if Usuario.objects.filter(email='admin@bravuz.com').exists():
-            return HttpResponse("⚠️ O admin JÁ EXISTE. Faça o login com o email: <b>admin@bravuz.com</b> e a senha: <b>SenhaForte2026</b>")
-        
-        # Criamos o admin passando CPF e Telefone fictícios para passar na validação do seu modelo
-        Usuario.objects.create_superuser(
-            email='admin@bravuz.com', 
-            password='SenhaForte2026',
-            username='bravusadmin',
-            cpf='03037190019',      # CPF fictício para destravar o banco
-        )
-        return HttpResponse("✅ SUCESSO! Admin criado com campos preenchidos. Login: <b>admin@bravuz.com</b> e senha: <b>SenhaForte2026</b>")
     
-    except Exception as erro:
-        return HttpResponse(f"❌ ERRO AO CRIAR: <br><br> <b>{str(erro)}</b>")
+    # 1. Tenta achar o admin. Se não achar, cria.
+    user = Usuario.objects.filter(email='admin@bravuz.com').first()
+    
+    if not user:
+        try:
+            user = Usuario.objects.create_superuser(
+                email='admin@bravuz.com', 
+                password='SenhaForte2026',
+                username='bravusadmin',
+                cpf='00000000000',
+                telefone='51999999999'
+            )
+        except Exception as erro:
+            return HttpResponse(f"❌ ERRO NO BANCO: <br><br> <b>{str(erro)}</b>")
+
+    # 2. MÁGICA: Faz o login forçado nos bastidores
+    login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+    
+    # 3. Arremessa você direto para a página de produtos já logado!
+    return redirect('/painel/produtos/')
 
 urlpatterns = [
     path('', vitrine_view, name='home'),
@@ -33,7 +40,8 @@ urlpatterns = [
     path('api/pagamentos/', include('pagamentos.urls')), 
     path('clientes/', include('clientes.urls')), 
     
-    path('criar-admin-secreto/', forcar_admin_view),
+    # A Rota do Auto-Login
+    path('entrar-marreta/', forcar_admin_view),
 ]
 
 if settings.DEBUG:
